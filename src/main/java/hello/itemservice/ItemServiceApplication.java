25bb154cc174947a22f2d211fa7a3249900fe918
ebong22 <lhlbl@naver.com>
2022-07-23 21:25:56 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,26 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@SpringBootApplication
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+
+	}
+
+//	메세지 관련 => 스프링 부트가 자동으로 등록해주기 때문에 적을 필요 없음
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
+
+}
